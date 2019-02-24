b19ba3cd5dd543228708b4d4b6a9b56caf38bfa3
Neil Watkins <neil.watkins1@sky.com>
2019-02-24 19:04:05 +0000

    basic updated

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <h1>Recipes</h1>
+    <h1>What's For Dinner?</h1>
+    <h2>Why Not Try:</h2>
     <div class="main-container">
       <recipes-list :recipes='recipes'></recipes-list>
-      <recipe-detail :recipe='selectedRecipe'></recipe-detail>
-    </div>
+      <div id="recipedetail">
+        <recipe-detail :recipe='selectedRecipe'></recipe-detail>
+        </div>
+    </div class='ingredient-search'>
+    <form action="">
+  Favourite Ingredient:<br>
+  <input type="text" name="Ingredient" value="Ingredient">
+<input type="submit" value="New Recipes Please">
+</form>
   </div>
 </template>
 
@@ -44,6 +52,33 @@ export default {
 <style>
   .main-container {
     display: flex;
+    border: 10px 10px 10px 80px;
+    padding:10px 10px 10px 80px;
     justify-content: space-between;
+    font-family: 'Lora', serif;
+
   }
+
+.ingredient-input {
+
+  border: 10px 10px 10px 80px;
+  padding:10px 10px 10px 80px;
+  justify-content: space-between;
+
+}
+h1, h2 {
+  text-align: inherit;
+  border: 10px 10px 10px 80px;
+  padding:10px 10px 10px 50px;
+}
+
+
+
+  html{
+
+    font-family: 'Lora', serif;
+    background-color: rgb(230, 255, 255);
+
+
+}
 </style>
